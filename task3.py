d47a2e33e8575b7c0d63784c4222fc34f730615c
Bredkina <bredkina_ag@mail.ru>
2021-02-28 23:27:49 +0300

    dz2

--- task3.py
+++ task3.py
@@ -1,10 +1,10 @@
 import yaml
 
-
-data_to_yaml = {'action':action_list, 'to':to_list}
+data_to_yaml = {'first': ["python", 3, "course"], 'second': 2,
+                "third": {"third1": "120€", "third2": "125€", "third3": "128€"}}
 
 with open('data_write.yaml', 'w') as f_n:
-    yaml.dump(data_to_yaml, f_n, default_flow_style=False, allow_unicode = True)
+    yaml.dump(data_to_yaml, f_n, default_flow_style=False, allow_unicode=True)
 
 with open('data_write.yaml') as f_n:
     print(f_n.read())
